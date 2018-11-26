f3cdfb95756c7dde8c602ef7e7265a9ccab48b8e
liumingjun <405702878@qq.com>
2018-11-26 22:52:53 +0800

    添加session

--- login/forms.py
+++ login/forms.py
@@ -9,7 +9,21 @@
 
 """
 from django import forms
+from captcha.fields import CaptchaField
 
 class UserForm(forms.Form):
     username = forms.CharField(max_length=255,label="用户名",widget=forms.TextInput(attrs={'class':'form-control'}))
     password = forms.CharField(max_length=255,label="密码",widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    captcha = CaptchaField(label="验证码")
+class RegisretForm(forms.Form):
+    gender =(
+        ('male','男'),
+        ('female','女')
+    )
+    username = forms.CharField(max_length=255,label="用户名",widget=forms.TextInput(attrs={'class':'form-control'}))
+    password1 = forms.CharField(max_length=255,label="密码",widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2 = forms.CharField(max_length=255,label="确认密码",widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    email = forms.EmailField(label="邮箱",widget=forms.EmailInput(attrs={'class':'form-control'}))
+    sex = forms.ChoiceField(label="性别",choices=gender)
+    mobile_phone = forms.CharField(max_length=11,label='手机号码',widget=forms.TextInput(attrs={'class':'form-control'}))
+    captcha = CaptchaField(label="验证码")
--- login/views.py
+++ login/views.py
@@ -8,6 +8,8 @@ def index(request):
     return render(request,'login/index.html')
 
 def login(request):
+    if request.session.get('is_login'):
+        return redirect('/index/')
     if request.method=="POST":
         login_form = forms.UserForm(request.POST)
         message = "所有字段都必须填写!"
@@ -17,6 +19,9 @@ def login(request):
             try:
                 user = models.User.objects.get(name=username)
                 if user.password == password:
+                    request.session['is_login'] = True
+                    request.session['user_name'] = user.name
+                    request.session['user_id'] = user.id
                     return redirect('/index/')
                 else:
                     message = "密码不正确！"
@@ -26,8 +31,46 @@ def login(request):
     login_form = forms.UserForm()
     return render(request,'login/login.html',locals())
 def register(request):
-    pass
-    return render(request,'login/register.html')
+    if request.session.get('is_login'):
+        return redirect('/index/')
+    if request.method == 'POST':
+        register_form = forms.RegisretForm(request.POST)
+        message = "请检查填写内容"
+        if register_form.is_valid():
+            username = register_form.cleaned_data['username']
+            password1 = register_form.cleaned_data['password1']
+            password2 = register_form.cleaned_data['password2']
+            email = register_form.cleaned_data['email']
+            sex = register_form.cleaned_data['sex']
+            mobile_phone = register_form.cleaned_data['mobile_phone']
+            if password1!=password2:
+                message = "两次输入的密码不同！"
+                return render(request,'login/register.html',locals())
+            else :
+                same_name = models.User.objects.filter(name=username)
+                if same_name:
+                    message = "用户名已存在，请输入别的用户名！"
+                    return render(request, 'login/register.html', locals())
+                same_email = models.User.objects.filter(email=email)
+                if same_email:
+                    message = "该邮箱地址已被注册，请使用别的邮箱！"
+                    return render(request, 'login/register.html', locals())
+                same_mobil_phone = models.User.objects.filter(mobile_phone=mobile_phone)
+                if same_mobil_phone:
+                    message = "该手机号码已被注册，请使用别的手机号码！"
+                    return render(request, 'login/register.html', locals())
+                new_user = models.User()
+                new_user.name = username
+                new_user.password = password1
+                new_user.email = email
+                new_user.sex = sex
+                new_user.mobile_phone = mobile_phone
+                new_user.save()
+                return redirect('/login/')
+    register_form = forms.RegisretForm()
+    return render(request,'login/register.html',locals())
 def logout(request):
-    pass
+    # if not request.session.get('is_login'):
+    #     return redirect('/index/')
+    request.session.flush()
     return redirect('/index/')
